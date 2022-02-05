53db7fdf07c1d1b64544078567b785345117b53a
Piotr Lewandowski <piotrl@Piotrs-MacBook-Pro.local>
2022-02-05 16:53:05 -0500

    setting up firebase, node packages, and credentials

---
+++ Week 3/databasesAPIs/weekend-study-pl/databasesAPIs.js
@@ -0,0 +1,11 @@
+/**
+ * 0. npm init in the folder you want to youse for the project
+ * 1. Install Firebase Packages: npm i firebase-admin
+ * 2. Import Firebase
+ * 3. Connect to the database
+ * 4. Access the collections
+ * 5. Read Documents
+ * 6. Insert Documents
+ * 7. Edit/Update Documents
+ * 
+ */
